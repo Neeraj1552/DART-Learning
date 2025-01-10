e2b1f122f33ada6fa3fa3a4be0483325e4cb8c49
Neeraj <neerajkeeriyad@gmail.com>
2025-01-10 12:08:05 +0530

    Add files via upload

---
+++ datatypes.dart
@@ -0,0 +1,12 @@
+void main() {
+  int num1 = 105;
+  double num2 = 130.8;
+  num num3 = 50.9;
+  double num4 = 50.7;
+  num Sum = num1 + num2 + num3 + num4;
+  print("num1 is $num1");
+  print("num2 is $num2");
+  print("num3 is $num3");
+  print("num4 is $num4");
+  print("sum is $Sum");
+}
