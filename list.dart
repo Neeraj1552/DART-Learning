ff049822246526677b7a9bca93fcad6770b6888f
Neeraj <neerajkeeriyad@gmail.com>
2025-01-10 12:08:25 +0530

    Add files via upload

---
+++ list.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> Fruits = ["orange", "grape", "mango"];
+  print("fruits are $Fruits");
+  Fruits.add("apple");
+  print(Fruits);
+  Fruits.insert(2, "Anar");
+  print(Fruits);
+}
