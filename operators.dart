4188bc1b08b67ed45ebc3332959f5cdb8267889e
Neeraj <neerajkeeriyad@gmail.com>
2025-01-12 20:39:28 +0530

    dart operators
    basics

---
+++ operators.dart
@@ -0,0 +1,16 @@
+void main() {
+  int num1 = 30;
+  int num2 = 10;
+
+  int sum = num1 + num2;
+  int diff = num1 - num2;
+  int Minus = -num2;
+  int mul = num1 * num2;
+  double div = num1 / num2;
+
+  print("The addition is $sum.");
+  print("The subtraction is $diff.");
+  print("The minus is $Minus.");
+  print("The multiplication is $mul.");
+  print("The division is $div.");
+}
