5521e20c8c08a589374ae86ae8c5153a55924bcd
Neeraj <neerajkeeriyad@gmail.com>
2025-01-12 20:59:29 +0530

    increment / decrement
    basics learning

---
+++ increment or decrement.dart
@@ -0,0 +1,13 @@
+void main() {
+  int num1 = 10;
+  int num2 = 20;
+
+  num2 = ++num1;
+  print("The value of num2 is $num2");
+
+  num1 = 10;
+  num2 = 20;
+
+  num2 = num1++;
+  print("The value of num2 is $num2");
+}
