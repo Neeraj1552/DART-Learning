e873e1e2a3f63ee584f16a16bcb934d8c0c1b1aa
Neeraj <neerajkeeriyad@gmail.com>
2025-01-22 23:59:02 +0530

    using class and  object in dart
    basics

---
+++ class and object.dart
@@ -0,0 +1,17 @@
+class Animal {
+  String? name;
+  int? numberOfLegs;
+
+  void display() {
+    print("Animal name: $name.");
+    print("Number of Legs: $numberOfLegs.");
+  }
+}
+
+void main() {
+  Animal animal = Animal();
+  animal.name = "Dog";
+  animal.numberOfLegs = 4;
+
+  animal.display();
+}
