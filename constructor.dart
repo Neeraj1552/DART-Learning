7cb96706b59892d0aeaa0d66e21557ab899aae2d
Neeraj <neerajkeeriyad@gmail.com>
2025-01-23 16:01:05 +0530

    using constructor in dart
    basics

---
+++ constructor.dart
@@ -0,0 +1,23 @@
+class Staff {
+  String? name;
+  int? phone1;
+  String? subject;
+
+  Staff(String name, int phone1, String subject) {
+    this.name = name;
+    this.phone1 = phone1;
+    this.subject = subject;
+  }
+
+  void display() {
+    print("Name: ${this.name}");
+    print("Phone1: ${this.phone1}");
+
+    print("Subject: ${this.subject}");
+  }
+}
+
+void main() {
+  Staff staff = Staff("John", 1234567890, "Maths");
+  staff.display();
+}
