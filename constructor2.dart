0a7e1f239931976358631dee1fefa7923130f9b9
Neeraj <neerajkeeriyad@gmail.com>
2025-01-24 23:12:10 +0530

    using  constructor
    basics

---
+++ constructor2.dart
@@ -0,0 +1,19 @@
+class Car {
+  String? name;
+  double? price;
+
+  Car(String name, double price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  void display() {
+    print("Name: ${this.name}");
+    print("Price: ${this.price}");
+  }
+}
+
+void main() {
+  Car car = Car("Ferrari", 10000000.0);
+  car.display();
+}
