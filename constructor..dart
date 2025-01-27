d135c6aaecb0d698a751ee2a09cb774f29d191b9
Neeraj <neerajkeeriyad@gmail.com>
2025-01-27 21:13:40 +0530

    Add files via upload

---
+++ constructor..dart
@@ -0,0 +1,24 @@
+class Student {
+  String? name; //class and properties
+  int? age;
+  int? rollnumb;
+  String? address;
+
+  Student(String name, int age, int rollnumb, String address) {
+    this.name = name;
+
+    //constructor used
+    this.age = age;
+    this.rollnumb = rollnumb;
+    this.address = address;
+  }
+}
+
+void main() {
+  Student student = Student("apple", 18, 32, "USA");
+
+  print("name is ${student.name}");
+  print("age is ${student.age}");
+  print("roll number is ${student.rollnumb}");
+  print("address is ${student.address}");
+}
