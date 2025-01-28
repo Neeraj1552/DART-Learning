2436a7be88b952406da6c5807a0d5f1ec46cb2e6
Neeraj <neerajkeeriyad@gmail.com>
2025-01-28 23:12:40 +0530

    getter and setter / encapsulation
    basics

---
+++ getter and setter.dart
@@ -0,0 +1,16 @@
+class Car {
+  String _name;
+
+  String get name => _name;
+  set name(String newName) => _name = newName;
+
+  Car(this._name);
+}
+
+void main() {
+  Car myCar = Car("Toyota");
+  print("Car name is: ${myCar.name}");
+
+  myCar.name = "benz";
+  print("Updated car name is: ${myCar.name}");
+}
