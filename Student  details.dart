ccac6f3daf21e84bf05327ab4c9f45ea8183f2a2
Neeraj <neerajkeeriyad@gmail.com>
2025-02-03 23:22:55 +0530

    Update Student  details.dart

--- Student  details.dart
+++ Student  details.dart
@@ -1,14 +1,15 @@
 class Student {
   String name, studentClass, gmail;
   int rollNumber;
+  int age;
 
-  Student(this.name, this.studentClass, this.rollNumber, this.gmail);
+  Student(this.name, this.studentClass, this.rollNumber, this.gmail,this.age);
 
   void display() =>
-      print("$name, $studentClass, Roll: $rollNumber, Gmail: $gmail");
+      print("$name, $studentClass, Roll: $rollNumber, Gmail: $gmail ,$age");
 }
 
 void main() {
-  Student student = Student('Amal', '10th grade', 16, 'amal123@gmail.com');
+  Student student = Student('Amal', '10th grade', 16, 'amal123@gmail.com,18');
   student.display();
 }
