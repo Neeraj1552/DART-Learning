d63ca4d4581771383a0f501ff2566c75d9008deb
Neeraj <neerajkeeriyad@gmail.com>
2025-02-15 10:41:10 +0530

    basics learning

---
+++ genericc.dart
@@ -0,0 +1,13 @@
+class Data<T> {
+  T data;
+  Data(this.data);
+}
+
+void main() {
+  // create an object of type int and double
+  Data<int> intData = Data<int>(10);
+  Data<double> doubleData = Data<double>(10.5);
+
+  print("IntData: ${intData.data}");
+  print("DoubleData: ${doubleData.data}");
+}
