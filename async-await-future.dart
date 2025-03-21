7cffb9fed4d830bf7a85dd79e33660b91a47e2bb
Neeraj <neerajkeeriyad@gmail.com>
2025-03-21 22:02:24 +0530

    Update async-await-future.dart

--- async-await-future.dart
+++ async-await-future.dart
@@ -3,7 +3,7 @@ Future<void> main() async {
   await Future.delayed(Duration(seconds: 5), () {
     print("second task completed ");
   });
-  Future.delayed(Duration(seconds: 4), () {
+  Future.delayed(Duration(seconds: 3), () {
     print("third task completed ");
   });
 }
