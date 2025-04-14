0c45e629b86e922a51e3c2336cc8f97807bfd093
Neeraj <neerajkeeriyad@gmail.com>
2025-04-14 20:08:47 +0530

    Update constructor..dart

--- constructor..dart
+++ constructor..dart
@@ -15,7 +15,7 @@ class Student {
 }
 
 void main() {
-  Student student = Student("apple", 48, 43, "UK");
+  Student student = Student("Gua", 48, 43, "UK");
 
   print("name is ${student.name}");
   print("age is ${student.age}");
