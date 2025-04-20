0af741b880feab34add95ca0692528036899f538
Neeraj <neerajkeeriyad@gmail.com>
2025-04-20 17:43:55 +0530

    Update constconstructor.dart

--- constconstructor.dart
+++ constconstructor.dart
@@ -9,7 +9,7 @@ class Car {
 
 void main() {
   // Here car is object of class Car.
-  const Car car = Car(name: "BMW", model: "X5", price: 50000);
+  const Car car = Car(name: "BMW", model: "X6", price: 50000);
   print("Name: ${car.name}");
   print("Model: ${car.model}");
   print("Price: ${car.price}");
